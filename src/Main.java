fb8858e0564dbeeb25c2a3d629da09abc30ba790
reksa ariansyah <reksa.ariansyah@gmail.com>
2022-10-21 19:28:18 +0700

    penambahan array

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-       int[] angkaAcak = {20, 35, -15, 7, 55, 1, -22};
+       int[] angkaAcak = {20, 35, -15, 7, 55, 1, -22, 80, -80};
 
        for (int lastIndex = angkaAcak.length - 1; lastIndex > 0;
             lastIndex--){
